748b6a962e6ef51a576b921545aad21ba6337209
Heidi Sprouse <hsprouse91@gmail.com>
2020-07-15 14:47:57 -0500

    code that was in the chapter now works

--- Gifter/Models/Comment.cs
+++ Gifter/Models/Comment.cs
@@ -15,6 +15,8 @@ namespace Gifter.Models
         [Required]
         public int PostId { get; set; }
 
+        public Post Post { get; set; }
+
         [Required]
         [StringLength(255, MinimumLength = 1)]
         public string Message { get; set; }
